8787b8351fdac388afc2c3bd5f0c15b78436bfc6
John Lewis <jblewis@internal.liveoakclassical.com>
2018-08-23 15:03:37 -0500

    First homework init commit

---
+++ src/hw1b/Stewie2.java
@@ -0,0 +1,10 @@
+package hw1b;
+
+public class Stewie2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("package hw1b;\n\npublic class Stewie2 {\n\n    public static void main(String[] args) {\n    // TODO Auto-generated method stub\n        System.out.println();\n    }\n\n}");
+	}
+
+}
