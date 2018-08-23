0657b1e193ffcbb90254a689f19547fa1a3e7db0
John Lewis <jblewis@internal.liveoakclassical.com>
2018-08-23 15:08:21 -0500

    added celebration line

--- src/hw1b/Stewie2.java
+++ src/hw1b/Stewie2.java
@@ -4,7 +4,7 @@ public class Stewie2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("package hw1b;\n\npublic class Stewie2 {\n\n    public static void main(String[] args) {\n    // TODO Auto-generated method stub\n        System.out.println();\n    }\n\n}");
+		System.out.println("package hw1b;\n\npublic class Stewie2 {\n\n    public static void main(String[] args) {\n    // TODO Auto-generated method stub\n        System.out.println();\n    }\n\n}\n\nYEAH");
 	}
 
 }
