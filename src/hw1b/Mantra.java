44fc00ff45895e4107e912bcc615b5f4883c230d
John Lewis <jblewis@internal.liveoakclassical.com>
2018-08-23 15:37:32 -0500

    Added Mantra program.

---
+++ src/hw1b/Mantra.java
@@ -0,0 +1,12 @@
+package hw1b;
+
+public class Mantra {
+	public static void main(String[] args) {
+		mantra();
+		mantra();
+	}
+	public static void mantra() {
+		System.out.println("There's one thing every coder must understand:");
+		System.out.println("The System.out.println command.");
+	}
+}
